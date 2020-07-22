2b17373f4783ca9742298132ef8dd6d8f8468aba
harukus <43bw.hana@gmail.com>
2020-07-22 15:57:48 +0900

    correspond validation error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,59 +1,45 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  # GET /books
-  # GET /books.json
   def index
     @books = Book.page(params[:page])
   end
 
-  # GET /books/1
-  # GET /books/1.json
   def show
   end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
   def edit
   end
 
-  # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: t(:create) }
-      else
-        format.html { render :new }
-      end
+    if @book.save
+      redirect_to @book, notice: t(:create)
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: t(:update) }
-      else
-        format.html { render :edit }
-      end
+    if @book.update(book_params)
+      redirect_to @book, notice: t(:update)
+    else
+      render :edit
     end
   end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: t(:destroy)  }
+    if @book.destroy
+      redirect_to books_url, notice: t(:destroy)
+    else
+      render :show
     end
+  end
+
   end
 
   private
