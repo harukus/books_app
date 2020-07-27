b9c18caa157cb8ce41feb0e2cf65ebf8217fa948
harukus <43bw.hana@gmail.com>
2020-07-27 16:30:46 +0900

    apply rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(p)
+  def after_sign_in_path_for(_p)
     books_path
   end
 
-  def after_sign_out_path_for(p)
+  def after_sign_out_path_for(_p)
     new_user_session_path
   end
 
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:email, :password, :password_confirmation, :postcode, :address, :biography]
+    added_attrs = %i[email password password_confirmation postcode address biography]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  def after_update_path_for(resource)
+  def after_update_path_for(_resource)
     user_path(current_user)
   end
 
