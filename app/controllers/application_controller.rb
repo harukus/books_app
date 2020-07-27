868f382dbb6ee87708f55419d3a0fc1cc45efc8f
harukus <43bw.hana@gmail.com>
2020-07-27 16:39:04 +0900

    Change argument names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(_p)
+  def after_sign_in_path_for(resource)
     books_path
   end
 
-  def after_sign_out_path_for(_p)
+  def after_sign_out_path_for(resource)
     new_user_session_path
   end
 
