c37c8fb0ab358132988189a153de3098567a5c55
harukus <43bw.hana@gmail.com>
2020-08-02 16:25:06 +0900

    add login&logout process

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
                                     sessions: 'users/sessions' }
   resources :books
   resources :users, only: [:show]
+  get '/auth/:provider/callback' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20_200_730_215_326) do
+ActiveRecord::Schema.define(version: 20_200_801_073_118) do
   create_table 'books', force: :cascade do |t|
     t.string 'title'
     t.text 'memo'
@@ -18,18 +17,6 @@ ActiveRecord::Schema.define(version: 20_200_730_215_326) do
     t.datetime 'updated_at', precision: 6, null: false
     t.string 'author'
     t.string 'picture'
-=======
-
-ActiveRecord::Schema.define(version: 2020_08_01_073118) do
-
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.text "memo"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "author"
-    t.string "picture"
->>>>>>> b3c7cc9... add column to users
   end
 
   create_table 'git_users', force: :cascade do |t|
@@ -40,8 +27,6 @@ ActiveRecord::Schema.define(version: 2020_08_01_073118) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
-
-<<<<<<< HEAD
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -53,26 +38,11 @@ ActiveRecord::Schema.define(version: 2020_08_01_073118) do
     t.string 'postcode'
     t.string 'address'
     t.text 'biography'
+    t.string 'provider', default: '', null: false
+    t.string 'uid', default: '', null: false
+    t.string 'name', default: '', null: false
+    t.string 'image_url', default: '', null: false
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-=======
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "postcode"
-    t.string "address"
-    t.text "biography"
-    t.string "provider", default: "", null: false
-    t.string "uid", default: "", null: false
-    t.string "name", default: "", null: false
-    t.string "image_url", default: "", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> b3c7cc9... add column to users
   end
 end
