6dc9df569c2c5d1c2536fec1681178607076932b
harukus <43bw.hana@gmail.com>
2020-08-02 16:26:31 +0900

    apply rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20_200_801_073_118) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
