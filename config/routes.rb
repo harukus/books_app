826d482f97eeaf4f9389ef4b6eaf647939420e70
harukus <43bw.hana@gmail.com>
2020-08-11 20:41:14 +0900

    delete unnecessary file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
-  #                                   sessions: 'users/sessions' }
   resources :books
   resources :users, only: [:show]
   get '/auth/:provider/callback' => 'sessions#create'
