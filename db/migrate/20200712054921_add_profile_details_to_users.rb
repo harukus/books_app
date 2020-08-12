e05812d118428660a57213044c3193a5ba867b6a
harukus <43bw.hana@gmail.com>
2020-08-13 08:05:46 +0900

    change file name because of the duplication

--- db/migrate/20200712054921_add_profile_details_to_users.rb
+++ db/migrate/20200712054921_add_profile_details_to_users.rb
@@ -1,4 +1,4 @@
-class AddDetailsToUsers < ActiveRecord::Migration[6.0]
+class AddProfileDetailToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :postcode, :string
     add_column :users, :address, :string
