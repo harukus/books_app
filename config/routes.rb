231b1718fd691ee20a069e7d81f0017773111042
harukus <43bw.hana@gmail.com>
2020-08-19 08:41:19 +0900

    creat userlist page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   resources :books
-  resources :users, only: [:show]
+  resources :users, only: %i[show index]
   get '/auth/:provider/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 end
