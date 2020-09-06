cfbc2178e5e91d429a22fa2e2cfc73d1bad5c53e
harukus <43bw.hana@gmail.com>
2020-09-06 17:37:52 +0900

    remake redirect url

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,35 @@
 class CommentsController < ApplicationController
+  before_action :set_commentable, only: %i[create update]
+
   def edit
     @comment = Comment.find(params[:id])
   end
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @commentable.comments.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_back(fallback_location: root_path)
+      redirect_to @commentable
     else
-      redirect_back(fallback_location: root_path)
+      render  ### ここがわからないです。
     end
   end
 
   def update
     @comment = Comment.find(params[:id])
     if @comment.update(comment_params)
-      redirect_to report_path(params[:report_id])
+      redirect_to @commentable
     else
-      redirect_back(fallback_location: root_path)
+      render :edit
     end
   end
 
   private
+
+  def set_commentable
+    resource, id = request.path.split('/')[1, 2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
 
   def comment_params
     params.require(:comment).permit(:content, :commentable_type, :commentable_id)
