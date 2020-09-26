5d6f608f465ae2ff683698b10b4abafbd695ec9d
harukus <43bw.hana@gmail.com>
2020-09-26 19:28:06 +0900

    make test about creating,updating, destroying Book

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -17,34 +17,33 @@ class BooksTest < ApplicationSystemTestCase
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
+    click_on '新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'メモ', with: @book.memo
+    fill_in 'タイトル', with: @book.title
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '作成しました。'
+    click_link '戻る'
   end
 
   test 'updating a Book' do
     visit books_url
-    click_on 'Edit', match: :first
+    click_on '編集', match: :first
 
     fill_in 'Memo', with: @book.memo
     fill_in 'Title', with: @book.title
     click_on 'Update Book'
 
     assert_text 'Book was successfully updated'
-    click_on 'Back'
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除', match: :first
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '削除しました。'
   end
 end
