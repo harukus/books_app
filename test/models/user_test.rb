c2c505c3af0f098c161651b73f26f352fb3fb3e2
harukus <43bw.hana@gmail.com>
2020-09-26 13:51:44 +0900

    make model test about method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name_or_email' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+  end
+
+  test 'unfollow' do
+    harukus = User.create!(email: 'harukus@example.com', password: 'password')
+    bump = User.create!(email: 'bump@example.com', password: 'password')
+
+    harukus.follow(bump.id)
+    assert harukus.following?(bump)
+    harukus.unfollow(bump.id)
+    assert_not harukus.following?(bump)
+  end
 end
