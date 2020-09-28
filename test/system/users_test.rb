f4f37bb6613fba5b8ca8f7a96be04999945d8b5d
harukus <43bw.hana@gmail.com>
2020-09-28 16:21:11 +0900

    make system test about follow/unfollow

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,31 @@
 require 'application_system_test_case'
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "Users"
-  # ends
+  setup do
+    visit root_url
+    fill_in 'Email', with: 'bump@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+  end
+
+  test 'follow user' do
+    visit root_path
+    click_link 'ユーザー一覧'
+    assert_text 'フォローする'
+
+    click_link 'フォローする'
+
+    assert_text 'フォロー外す'
+  end
+
+  test 'unfollow user' do
+    visit root_path
+    click_link 'ユーザー一覧'
+    click_link 'フォローする'
+
+    assert_text 'フォロー外す'
+    click_link 'フォロー外す'
+
+    assert_text 'フォローする'
+  end
 end
